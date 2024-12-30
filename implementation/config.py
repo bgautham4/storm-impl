e01531652c472a763477af03f0088a0f3ae303f3
bgautham4 <bgautham4@gmail.com>
2024-12-30 10:42:30 +0530

    Change to pyhon3.x code from 2.x

--- implementation/config.py
+++ implementation/config.py
@@ -2,20 +2,24 @@ import sys
 import socket
 import struct
 import netifaces as ni
-# ether_addrs = ["00:01:e8:8b:2e:e4", "00:01:e8:8b:2e:e4", "00:01:e8:8b:2e:e4", "00:01:e8:8b:2e:e4", "00:01:e8:8b:2e:e4", "00:01:e8:8b:2e:e4", "00:01:e8:8b:2e:e4", "00:01:e8:8b:2e:e4"]
 ether_addrs = []
-def construct_ip(small_ip, large_ip, ip_prefix = "10,10,1"):
+
+
+def construct_ip(small_ip, large_ip, ip_prefix="10,10,1"):
     num_dst = large_ip - small_ip + 1
     dst_ips = ""
     for i in range(num_dst):
-        dst_ips += "\tp->dst_ips[{}] = IPv4({}, {});".format(i, ip_prefix, small_ip + i)
+        dst_ips += "\tp->dst_ips[{}] = IPv4({}, {});".format(i,
+                                                             ip_prefix, small_ip + i)
         dst_ips += "\n"
     return num_dst, dst_ips
+
 
 def ip2int(addr):
     return struct.unpack("!I", socket.inet_aton(addr))[0]
 
-def read_arp_and_ip(file = "/proc/net/arp"):
+
+def read_arp_and_ip(file="/proc/net/arp"):
     f = open(file, "r")
     lines = f.readlines()[1:]
     dict_ip = {}
@@ -26,15 +30,16 @@ def read_arp_and_ip(file = "/proc/net/arp"):
         if "10.10" in ip:
             dict_ip[ip2int(ip)] = eth
 
-    # read ip 
+    # read ip
     ip = ni.ifaddresses('eno1d1')[ni.AF_INET][0]['addr']
     ether = ni.ifaddresses('eno1d1')[ni.AF_LINK][0]['addr']
     dict_ip[ip2int(ip)] = ether
 
     for key in sorted(dict_ip.keys()):
         ether_addrs.append(dict_ip[key])
-    print ether_addrs
+    print(ether_addrs)
     return ip
+
 
 def construct_ethers():
     i = 0
@@ -43,10 +48,12 @@ def construct_ethers():
         parts = addr.split(":")
         j = 0
         for p in parts:
-            output += "\tp->dst_ethers[{}].addr_bytes[{}] = {};\n".format(i, j, "0x" + p.upper())
+            output += "\tp->dst_ethers[{}].addr_bytes[{}] = {};\n".format(
+                i, j, "0x" + p.upper())
             j += 1
         i += 1
     return output
+
 
 def main():
     small_ip = sys.argv[1]
@@ -55,7 +62,7 @@ def main():
     # config_string.format(ip_str)
     ip = read_arp_and_ip()
     index = int(ip.split(".")[3])
-    ip_str =  "IPv4(" + ip.replace(".", ",") + ")"
+    ip_str = "IPv4(" + ip.replace(".", ",") + ")"
     config_string = """
 #include "config.h"
 #include <rte_ip.h>
@@ -66,7 +73,7 @@ struct Params params = {{
     .small_flow_thre = 20,
     .mss = 1460,
     .priority_limit = 6,
-    .bandwidth = 10000000000,
+    .bandwidth = 10000000000, //10Gbps
     .ip = {1},
     .pim_beta = 5,
     .pim_alpha = 1.1,
@@ -85,7 +92,7 @@ struct Params params = {{
     statement = dst_ips
     statement += construct_ethers()
     statement += "\tparams.token_window_timeout_cycle = (uint64_t) (params.token_window_timeout * params.BDP * 1500 * 8 \n \t / params.bandwidth * rte_get_timer_hz());\n"
-    init_string= """
+    init_string = """
 void init_config(struct Params* p) {{
 {0}
 }}
@@ -95,4 +102,6 @@ void init_config(struct Params* p) {{
     f.write(config_string)
     f.write(init_string)
     f.close()
+
+
 main()
