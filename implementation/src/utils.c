98786a5b620bb1cc43e4fe05a3ba8b52bd2ddede
bgautham4 <bgautham4@gmail.com>
2025-03-12 16:38:49 +0530

    Fisher Yates shuffle algo for inplace shuffling of arrays

--- implementation/src/utils.c
+++ implementation/src/utils.c
@@ -3,22 +3,17 @@
 #include <string.h>
 #include <stdlib.h>
 
-void shuffle_inplace(void *list, size_t n, size_t size) {
-    char tmp[size];
-    char *arr = list;
-    size_t stride = size * sizeof(char);
-
+void shuffle_inplace(void *array, size_t n, size_t elsize) {
     if (n <= 1) {
-        return;
+            return;
     }
-
-    size_t i;
-    for (i = 0; i < n - 1; ++i) {
-        size_t rnd = (size_t) rand();
-        size_t j = i + rnd / (RAND_MAX / (n - i) + 1);
-
-        memcpy(tmp, arr + j * stride, size);
-        memcpy(arr + j * stride, arr + i * stride, size);
-        memcpy(arr + i * stride, tmp, size);
+    char tmp[elsize];
+    char *arr = array;
+    size_t stride = elsize * sizeof(char);
+    for (size_t i = n-1; i > 0; --i) {
+            size_t j = ((size_t) rand()) % (i + 1);
+            memcpy(tmp, arr + i * stride, elsize);
+            memcpy(arr + i * stride, arr + j * stride, elsize);
+            memcpy(arr + j * stride, tmp, elsize);
     }
 }
