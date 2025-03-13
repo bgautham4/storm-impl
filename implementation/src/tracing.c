9b201fe2d073550e13429162fb757bd81e61a67e
bgautham4 <bgautham4@gmail.com>
2025-03-13 16:16:12 +0530

    Counters for tracing control packet counts and matches

---
+++ implementation/src/tracing.c
@@ -0,0 +1,36 @@
+#include "tracing.h"
+#include <stdio.h>
+
+CtrlPacketCounter ctrl_pkt_cntr = {{0}};
+
+void incr_ctrl_packet_count(CtrlPacketCounter *cntr, ctrlpacket_t type) {
+	if (type >= NUM_CTRL_PACKET_TYPES) {
+		return;
+	}	
+	cntr->packet_counts[type] += 1;
+}
+
+// Dump ctrl packet counts onto stdout
+void dump_ctrl_packet_counts(const CtrlPacketCounter *cntr) {
+	const char *packet_names[NUM_CTRL_PACKET_TYPES] = {
+		"SYNC", "SYNC_ACK", "PERMIT", "RTS", "GRANT", "ACCEPT", "GRANTR"
+	};
+
+	printf("Control Packet Counts:\n");
+	for (int i = 0; i < NUM_CTRL_PACKET_TYPES; i++) {
+		printf("%s: %lu\n", packet_names[i], cntr->packet_counts[i]);
+	}
+}
+
+MatchCounter match_cntr = {0ul,0ul};
+
+void incr_match_count(MatchCounter *cntr, unsigned long epoch) {
+	if (epoch > cntr->last_epoch) {
+		cntr->last_epoch = epoch;
+	}
+	cntr->num_matches += 1;
+}
+
+void dump_match_count(const MatchCounter *cntr) {
+	printf("TOT_EPOCHS_SEEN: %lu\nTOT_MATCHES: %lu\n", cntr->last_epoch, cntr->num_matches);
+}
---
+++ implementation/src/tracing.h
@@ -0,0 +1,41 @@
+#ifndef TRACING_H
+#define TRACING_H
+
+
+#define NUM_CTRL_PACKET_TYPES 7
+
+typedef enum ctrlpacket_t {
+	SYNC=0,
+	SYNCS_ACK,
+	PERMIT,
+	RTS,
+	GRANT,
+	ACCEPT,
+	GRANTR
+} ctrlpacket_t;
+
+/*
+For tracking number of control packets transmitted by each host
+*/
+typedef struct CtrlPacketCounter {
+	unsigned long packet_counts[NUM_CTRL_PACKET_TYPES];		 
+} CtrlPacketCounter;
+
+extern CtrlPacketCounter ctrl_pkt_cntr;
+
+void incr_ctrl_packet_count(CtrlPacketCounter *cntr, ctrlpacket_t type);
+void dump_ctrl_packet_counts(const CtrlPacketCounter *cntr);
+
+/*
+For tracking the number of matches
+*/
+typedef struct MatchCounter {
+	unsigned long last_epoch;
+	unsigned long num_matches;
+} MatchCounter;
+
+extern MatchCounter match_cntr;
+
+void incr_match_count(MatchCounter *cntr, unsigned long epoch);
+void dump_match_count(const MatchCounter *cntr);
+#endif
